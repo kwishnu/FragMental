fa6239af50066a1bc6dff7205ff97ba85a703486
kwishnu <kwish1777@gmail.com>
2017-03-08 08:58:23 -0800

    First page app intro...

--- app/routes/start_scene.js
+++ app/routes/start_scene.js
@@ -10,11 +10,9 @@ import {
   AsyncStorage,
   BackAndroid
 } from 'react-native';
-//import Meteor from 'react-native-meteor';
-const windowsWidth = Dimensions.get('window').width;
-const windowsHeight = Dimensions.get('window').height;
 import AppIntro from 'react-native-app-intro';
 var KEY_UseNumLetters = 'numLetters';
+var {width, height} = require('Dimensions').get('window');
 
 
 class StartScene extends Component {
@@ -72,172 +70,129 @@ class StartScene extends Component {
         this.goSomewhere();
 
     }
-    nextBtnHandle = () => {
-        //Alert.alert('Next');
-        //console.log(index);
-    }
-    onSlideChangeHandle = () => {
-        //console.log(index, total);
-
-    }
 
     render() {
-
-//#fa931d
-    return (
-      <AppIntro
-        onNextBtnClick={this.nextBtnHandle}
-        onDoneBtnClick={this.doneBtnHandle}
-        onSkipBtnClick={this.onSkipBtnHandle}
-        onSlideChange={this.onSlideChangeHandle}
-      >
-      <View style={[styles.slide,{ backgroundColor: '#486bdd' }]}>
-        <View style={[styles.header, {width: windowsWidth}]}>
-          <View>
-            <Image style={{ width: 75 * 2.5, height: 63 * 2.5 }} source={require('../images/1/c1.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 80,
-            left: 30,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={20}
-          >
-            <Image style={{ width: 46 * 2.5, height: 28 * 2.5 }} source={require('../images/1/c2.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 23,
-            left: 25,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={20}
-          >
-            <Image style={{ width: 109 * 2.5, height: 68 * 2.5 }} source={require('../images/1/c5.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 65,
-            left: 35,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={5}
-          >
-            <Image style={{ width: 23 * 2.5, height: 17 * 2.5 }} source={require('../images/1/c3.png')} />
-          </View>
-        </View>
-        <View style={styles.info}>
-          <View level={15}><Text style={styles.description}>Welcome to</Text></View>
-          <View level={10}><Text style={styles.title}>FragMental!</Text></View>
-          <View level={15}><Text style={styles.description}>Swipe through for a quick tutorial...</Text></View>
-        </View>
-      </View>
-      <View style={[styles.slide, { backgroundColor: '#a4b602' }]}>
-        <View style={[styles.header, {width: windowsWidth}]}>
-          <View>
-            <Image style={{ width: 75 * 2.5, height: 63 * 2.5 }} source={require('../images/2/1.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 30,
-            left: 40,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={20}
-          >
-            <Image style={{ width: 101 * 2.5, height: 71 * 2.5 }} source={require('../images/2/2.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 10,
-            left: 50,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={-20}
-          >
-            <Image style={{ width: 85 * 2.5, height: 73 * 2.5 }} source={require('../images/2/3.png')} />
-          </View>
-        </View>
-        <View style={styles.info}>
-          <View level={10}><Text style={styles.title}>Title!</Text></View>
-          <View level={15}><Text style={styles.description}>description!</Text></View>
-        </View>
-      </View>
-      <View style={[styles.slide, { backgroundColor: '#406E9F' }]}>
-        <View style={[styles.header, {width: windowsWidth}]}>
-          <View style={{
-            position: 'absolute',
-            top: 20,
-            left: 20,
-            width: windowsWidth,
-            height: windowsHeight,
-          }}
-          >
-            <Image style={{ width: 138 * 2.5, height: 83 * 2.5 }} source={require('../images/3/3.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 25,
-            left: 40,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={-15}
-          >
-            <Image style={{ width: 103 * 2.5, height: 42 * 2.5 }} source={require('../images/3/4.png')} />
-          </View>
-          <View level={10}>
-            <Image style={{ width: 95 * 2.5, height: 55 * 2.5 }} source={require('../images/3/1.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 65,
-            left: 120,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={25}
-          >
-            <Image style={{ width: 47 * 2.5, height: 43 * 2.5 }} source={require('../images/3/2.png')} />
-          </View>
-        </View>
-        <View style={styles.info}>
-          <View level={10}><Text style={styles.title}>Title!</Text></View>
-          <View level={15}><Text style={styles.description}>description!</Text></View>
-        </View>
-      </View>
-      <View style={[styles.slide, { backgroundColor: '#DB4302' }]}>
-        <View style={[styles.header, {width: windowsWidth}]}>
-          <View style={{
-            position: 'absolute',
-            top: 25,
-            left: 55,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={15}
-          >
-            <Image style={{ width: 96 * 2.5, height: 69 * 2.5 }} source={require('../images/4/4.png')} />
-          </View>
-          <View>
-            <Image style={{ width: 50 * 2.5, height: 63 * 2.5 }} source={require('../images/4/1.png')} />
-          </View>
-          <View style={{
-            position: 'absolute',
-            top: 20,
-            left: 70,
-            width: windowsWidth,
-            height: windowsHeight,
-          }} level={20}
-          >
-            <Image style={{ width: 46 * 2.5, height: 98 * 2.5 }} source={require('../images/4/3.png')} />
-          </View>
-        </View>
-        <View style={styles.info}>
-          <View level={10}><Text style={styles.title}>Title!</Text></View>
-          <View level={15}><Text style={styles.description}>description!</Text></View>
-        </View>
-      </View>
-      </AppIntro>
-    );
+        return (
+            <AppIntro onDoneBtnClick={this.doneBtnHandle} onSkipBtnClick={this.onSkipBtnHandle}>
+                <View style={[styles.slide,{ backgroundColor: '#081262' }]}>
+                    <View style={styles.header}>
+                        <View style={[styles.pic, {top: -10, left: width*.05,}]} level={-15}>
+                            <Image style={{ width: width*.7, height: width*.7 }} source={require('../images/intro1/gradient2.png')} />
+                        </View>
+                        <View style={[styles.pic, {top: width*.05, left: width*.05,}]} level={20}>
+                            <Image style={{ width: width*.33, height: width*.33 }} source={require('../images/intro1/frag2.png')} />
+                        </View>
+                        <View style={[styles.pic, {top: width*.05, left: width*.25,}]} level={-30}>
+                            <Image style={{ width: width*.33, height: width*.33 }} source={require('../images/intro1/frag3.png')} />
+                        </View>
+                        <View style={[styles.pic, {top: width*.11, left: width*.08,}]} level={5}>
+                            <Image style={{ width: width*.33, height: width*.33 }} source={require('../images/intro1/frag4.png')} />
+                        </View>
+                        <View style={[styles.pic, {top: width*.19, left: width*.26,}]} level={-10}>
+                            <Image style={{ width: width*.33, height: width*.33 }} source={require('../images/intro1/frag5.png')} />
+                        </View>
+                        <View style={[styles.pic, {top: width*.02, left: width*.16,}]} level={5}>
+                            <Image style={{ width: width*.33, height: width*.33 }} source={require('../images/intro1/frag6.png')} />
+                        </View>
+                        <View style={[styles.pic, {top: width*.13, left: width*.2,}]} level={10}>
+                            <Image style={{ width: width*.33, height: width*.33 }} source={require('../images/intro1/frag7.png')} />
+                        </View>
+                        <View>
+                            <Image style={{ width: width*.33, height: width*.33 }} source={require('../images/intro1/frag1.png')} />
+                        </View>
+                    </View>
+                    <View style={styles.info}>
+                        <View level={15}><Text style={styles.description}>Welcome to</Text></View>
+                        <View level={10}><Text style={styles.title}>FragMental!</Text></View>
+                        <View style={styles.center_text_view}>
+                            <View level={0}><Text style={styles.description}>Swipe through for a quick tutorial...</Text></View>
+                        </View>
+                    </View>
+                </View>
+                <View style={[styles.slide, { backgroundColor: '#486bdd' }]}>
+                    <View style={styles.header}>
+                        <View>
+                            <Image style={{ width: 75 * 2.5, height: 63 * 2.5 }} source={require('../images/2/1.png')} />
+                        </View>
+                        <View style={[styles.pic, {
+                        top: 30,
+                        left: 40,
+                        }]} level={20}
+                        >
+                            <Image style={{ width: 101 * 2.5, height: 71 * 2.5 }} source={require('../images/2/2.png')} />
+                        </View>
+                        <View style={[styles.pic, {
+                        top: 10,
+                        left: 50,
+                        }]} level={-20}
+                        >
+                            <Image style={{ width: 85 * 2.5, height: 73 * 2.5 }} source={require('../images/2/3.png')} />
+                        </View>
+                    </View>
+                    <View style={styles.info}>
+                        <View level={10}><Text style={styles.title}>Title!</Text></View>
+                        <View level={15}><Text style={styles.description}>description!</Text></View>
+                    </View>
+                </View>
+                <View style={[styles.slide, { backgroundColor: '#406E9F' }]}>
+                    <View style={styles.header}>
+                    <View style={[styles.pic, {
+                    top: 20,
+                    left: 20,
+                    }]}
+                    >
+                    <Image style={{ width: 138 * 2.5, height: 83 * 2.5 }} source={require('../images/3/3.png')} />
+                    </View>
+                    <View style={[styles.pic, {
+                    top: 25,
+                    left: 40,
+                    }]} level={-15}
+                    >
+                    <Image style={{ width: 103 * 2.5, height: 42 * 2.5 }} source={require('../images/3/4.png')} />
+                    </View>
+                    <View level={10}>
+                    <Image style={{ width: 95 * 2.5, height: 55 * 2.5 }} source={require('../images/3/1.png')} />
+                    </View>
+                    <View style={[styles.pic, {
+                    top: 65,
+                    left: 120,
+                    }]} level={25}
+                    >
+                    <Image style={{ width: 47 * 2.5, height: 43 * 2.5 }} source={require('../images/3/2.png')} />
+                    </View>
+                    </View>
+                    <View style={styles.info}>
+                    <View level={10}><Text style={styles.title}>Title!</Text></View>
+                    <View level={15}><Text style={styles.description}>description!</Text></View>
+                    </View>
+                </View>
+                <View style={[styles.slide, { backgroundColor: '#081262' }]}>
+                    <View style={styles.header}>
+                        <View>
+                            <Image style={{ width: 75 * 2.5, height: 63 * 2.5 }} source={require('../images/2/1.png')} />
+                        </View>
+                        <View style={[styles.pic, {
+                        top: 30,
+                        left: 40,
+                        }]} level={20}
+                        >
+                            <Image style={{ width: 101 * 2.5, height: 71 * 2.5 }} source={require('../images/2/2.png')} />
+                        </View>
+                        <View style={[styles.pic, {
+                        top: 10,
+                        left: 50,
+                        }]} level={-20}
+                        >
+                            <Image style={{ width: 85 * 2.5, height: 73 * 2.5 }} source={require('../images/2/3.png')} />
+                        </View>
+                    </View>
+                    <View style={styles.info}>
+                        <View level={10}><Text style={styles.title}>Title!</Text></View>
+                        <View level={15}><Text style={styles.description}>description!</Text></View>
+                    </View>
+                </View>
+            </AppIntro>
+        );
     }
 }
 
@@ -250,23 +205,19 @@ const styles = StyleSheet.create({
     padding: 15,
   },
   header: {
-    flex: 0.5,
+    flex: 1,
+    width: width,
     justifyContent: 'center',
     alignItems: 'center',
   },
   pic: {
-    width: 75 * 2,
-    height: 63 * 2,
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
+    position: 'absolute',
+    width: width,
+    height: height,
   },
   info: {
-    flex: 0.5,
+    flex: 1,
     alignItems: 'center',
-    padding: 40
   },
   title: {
     color: '#fff',
@@ -276,11 +227,16 @@ const styles = StyleSheet.create({
   description: {
     color: '#fff',
     fontSize: 20,
+    textAlign: 'center',
   },
-  separator: {
-      flex: 1,
-      height: StyleSheet.hairlineWidth,
-      backgroundColor: '#8E8E8E',
+  center_text_view: {
+    flex: 1,
+    width: width*.7,
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginBottom: 40
   },
 });
 
