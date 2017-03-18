3642945d2bdb3760a1961d724ab37dae06e16fa2
kwishnu <kwish1777@gmail.com>
2017-03-18 15:12:09 -0700

    Styled for tablet compatibility

--- app/config/configs.js
+++ app/config/configs.js
@@ -1,9 +1,28 @@
+import React, { Component } from 'react';
+
+var {width, height} = require('Dimensions').get('window');
+var CELL_WIDTH = Math.floor(height/1.84)/4;
+var CELL_HEIGHT = CELL_WIDTH * .55;
+var CELL_PADDING = Math.floor(CELL_WIDTH * .06);
+var BORDER_RADIUS = CELL_PADDING * .2;
+var TILE_WIDTH = CELL_WIDTH - CELL_PADDING * 2;
+var TILE_HEIGHT = CELL_HEIGHT - CELL_PADDING * 2;
+var LETTER_SIZE = Math.floor(TILE_HEIGHT * .7);
 
 
 export const configs = {
     versionCode: 4,
     versionName: '1.0.4',
-    appStoreID: 'com.fragmental'
+    appStoreID: 'com.fragmental',
+    NUM_WIDE: 4,
+    CELL_WIDTH: CELL_WIDTH,
+    CELL_HEIGHT: CELL_HEIGHT,
+    CELL_PADDING: CELL_PADDING, // 8% of the cell width
+    BORDER_RADIUS: BORDER_RADIUS,
+    TILE_WIDTH: TILE_WIDTH,
+    TILE_HEIGHT: TILE_HEIGHT,
+    LETTER_SIZE: LETTER_SIZE
+
 };
 
 export default configs;
--- app/styles/styles.js
+++ app/styles/styles.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-
-//const window = Dimensions.get('window');
-
+import configs from '../config/configs';
 var { width, height } = require('Dimensions').get('window');
-var NUM_WIDE = 4;
-var NUM_HIGH = 5;
-var CELL_WIDTH = Math.floor(width * .24); // 24% of the screen width
-var CELL_HEIGHT = CELL_WIDTH * .55;
-var CELL_PADDING = Math.floor(CELL_WIDTH * .08); // 8% of the cell width
-var BORDER_RADIUS = CELL_PADDING * .2;
-var TILE_WIDTH = CELL_WIDTH - CELL_PADDING * 2;
-var TILE_HEIGHT = CELL_HEIGHT - CELL_PADDING * 2;
-var LETTER_SIZE = Math.floor(TILE_HEIGHT * .7);
-
 
 module.exports = StyleSheet.create({
-    tile: {
+    cell: {
         position: 'absolute',
-        width: TILE_WIDTH,
-        height: TILE_HEIGHT,
-        borderRadius: BORDER_RADIUS,
+        width: configs.CELL_WIDTH,
+        height: configs.CELL_HEIGHT,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+    },
+    tile: {
+        width: configs.TILE_WIDTH,
+        height: configs.TILE_HEIGHT,
+        borderRadius: configs.BORDER_RADIUS,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#ffffff',
-        padding: 8,
     },
     skip_button: {
         flex:1,
@@ -48,31 +42,35 @@ module.exports = StyleSheet.create({
     },
     puzzle_text_large: {
         color: '#000',
-        fontSize: LETTER_SIZE,
+        fontSize: configs.LETTER_SIZE,
     },
     contents_text: {
         color: '#fff',
-        fontSize: LETTER_SIZE,
+        fontSize: configs.LETTER_SIZE,
     },
     answer_text: {
-        fontSize: LETTER_SIZE * 5/3,
+        fontSize: configs.LETTER_SIZE * 1.5,
         fontWeight: 'bold',
     },
     answer_column_text: {
         color: 'white',
-        fontSize: LETTER_SIZE * 2/3,
+        fontSize: configs.LETTER_SIZE * 0.7,
     },
     clue_text_bold: {
-        fontSize: 16,
+        fontSize: configs.LETTER_SIZE * 0.6,
         fontWeight: 'bold',
         textAlign: 'center',
     },
     clue_text: {
-        fontSize: 16,
+        fontSize: configs.LETTER_SIZE * 0.5,
     },
     keyfrag_text: {
         color: '#038c30',
-        fontSize: LETTER_SIZE * 4/3,
+        fontSize: configs.LETTER_SIZE * 4/3,
+        fontWeight: 'bold',
+    },
+    score_text: {
+        fontSize: configs.LETTER_SIZE * 5/3,
         fontWeight: 'bold',
     },
     input_box: {
@@ -92,10 +90,10 @@ module.exports = StyleSheet.create({
     },
     header_text: {
         color: '#e3e004',
-        fontSize: 18,
+        fontSize: configs.LETTER_SIZE * 0.6,
     },
     daily_launcher_text: {
-        fontSize: 14,
+        fontSize: configs.LETTER_SIZE * 0.5,
     },
     menu: {
         flex: 1,
@@ -103,43 +101,6 @@ module.exports = StyleSheet.create({
         height: height,
         backgroundColor: 'blue',
         padding: 20,
-    },
-    menu_font: {
-        color: '#fff',
-        fontSize: 40,
-    },
-    caption: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        alignItems: 'center',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    avatarContainer: {
-        marginBottom: 20,
-        marginTop: 20,
-    },
-    avatar: {
-        width: 48,
-        height: 48,
-        borderRadius: 24,
-        flex: 1,
-    },
-    name: {
-        color: '#fff',
-        fontSize: 20,
-    },
-    menu_item: {
-        fontSize: 20,
-        color: '#fff',
     },
     copyright: {
         fontSize: 10,
