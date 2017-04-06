44a87f46ece829479bbb3a90f9fbe206ea50257a
kwishnu <kwish1777@gmail.com>
2017-04-06 13:37:19 -0700

    Changed some font sizes

--- app/styles/styles.js
+++ app/styles/styles.js
@@ -20,28 +20,6 @@ module.exports = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#ffffff',
     },
-    skip_button: {
-        flex:1,
-        height: height * 0.075,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#64aefa',
-        borderWidth: 1,
-        borderRadius: 12,
-        marginRight: 3,
-        padding: 12,
-    },
-    hint_button: {
-        flex:1,
-        height: height * 0.075,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#4aeeb2',
-        borderWidth: 1,
-        borderRadius: 12,
-        marginLeft: 3,
-        padding: 12,
-    },
     puzzle_text_large: {
         color: '#000',
         fontSize: configs.LETTER_SIZE * 0.9,
@@ -51,12 +29,12 @@ module.exports = StyleSheet.create({
         fontSize: configs.LETTER_SIZE,
     },
     answer_text: {
-        fontSize: configs.LETTER_SIZE * 1.5,
+        fontSize: configs.LETTER_SIZE * 1.1,
         fontWeight: 'bold',
     },
     answer_column_text: {
         color: 'white',
-        fontSize: configs.LETTER_SIZE * 0.7,
+        fontSize: configs.LETTER_SIZE * 0.6,
     },
     clue_text_bold: {
         fontSize: configs.LETTER_SIZE * 0.6,
@@ -68,31 +46,16 @@ module.exports = StyleSheet.create({
     },
     keyfrag_text: {
         color: '#038c30',
-        fontSize: configs.LETTER_SIZE * 4/3,
+        fontSize: configs.LETTER_SIZE * 1.1,
         fontWeight: 'bold',
     },
     score_text: {
         fontSize: configs.LETTER_SIZE * 5/3,
         fontWeight: 'bold',
     },
-    input_box: {
-        flex: 1,
-        borderWidth: 2,
-        borderColor: '#000',
-        borderRadius: 5,
-        backgroundColor: '#ccd5ee',
-    },
-    input_button: {
-        flex: 1,
-        borderWidth: 4,
-        borderColor: '#087e2f',
-        borderRadius: 0,
-        backgroundColor: '#087e2f',
-        margin: 6,
-    },
     header_text: {
         color: '#e3e004',
-        fontSize: configs.LETTER_SIZE * 0.6,
+        fontSize: configs.LETTER_SIZE * 0.7,
     },
     daily_launcher_text: {
         fontSize: configs.LETTER_SIZE * 0.5,
